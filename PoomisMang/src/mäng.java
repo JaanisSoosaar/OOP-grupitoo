0b64370a8085d4fac7148957cdaa1d74f7567cea
Jaanis Soosaar <93191144+Jaaniss@users.noreply.github.com>
2023-03-19 00:36:40 +0200

    Esimene upload
    Esimesed algsed failid koos kirjeldustega

---
+++ PoomisMang/src/mäng.java
@@ -0,0 +1,10 @@
+public class mäng {
+    /*
+    TODO:
+        - siin klassis võiksid olla isendiväljad, konstruktorid, get- ja set-meetodid ja teised vajalikud meetodid.
+        -
+        - Oleks näiteks isik Mängija
+
+        - Kindlasti võiks olemas olla privaatne muutuja, kus salvestatakse pakutud tähed.
+     */
+}
---
+++ PoomisMang/src/sõnaArvamine.java
@@ -0,0 +1,72 @@
+public class sõnaArvamine {
+
+    /*
+    TODO:
+        - Äkki teeks kolm massiivi, kus on kõik n - pikkused sõnad sees. Valikuteks äkki 3, 4 ja 5.
+          sõnadFaili.java abil
+        -
+          Teisipäeval, kui kokku saame, siis võin näidata, kuidas ma neid massiive teeksin.
+        - Siis mängija saaks mängu alguses valida, kui pikka sõna ta tahaks ära arvama hakata.
+        - Siis me valiks vastavast massiivist suvaliselt ühe n - pikkuse sõna ning salvestaksime selle.
+        -
+        - Ja kui inimene pakub tähe, siis võrdleme tema pakkumist ning meie suvalist sõna.
+         Kui sõnas on pakutud täht, siis anname mängijale teada, et ta pakkus õigesti.
+         Kui ei ole, siis ütleme, et pakutud tähte sõnas ei ole.
+         Mängija saab pakkuda, kuni sõna on ära arvatud.
+
+
+        TODO-- Kuidagi peaksime isendiväljad, konstruktorid, get- ja set-meetodid jne ka koodis hõlmama,
+          sest see on juhendis kirjas.
+
+
+    TODO:
+        Mäng töötaks niimoodi:
+        ----------------------------------------------------------------------
+        Arvuti: Mitme tähe pikkust sõna soovid arvata? (Näiteks 3 - 5)
+        Mängija: 4
+        -
+        **Arvuti valib taustal suvalise 4 tähe pikkuse sõna: Näiteks "kool".
+        -
+        Arvuti: Palun siseta täht.
+        Mängija: a
+        -
+        **Arvuti kontrollib, kas pakutud täht on sõnas. Vastuseks saab false, sest sõnas "kool" ei ole ühtegi a tähte.
+        -
+        Arvuti: Teie pakutud tähte ei esine valitud sõnas! : ****
+        Arvuti: Olete pakkunud 1 korra. Ning pakkunud tähti : a
+        Arvuti: Palun paku uus täht.
+        Mängija: o
+         -
+        **Arvuti kontrollib, kas pakutud täht on sõnas. Vastuseks saab true, sest sõnas "kool" on vähemalt 1 o täht.
+        -
+        Arvuti: Teie pakutud täht esines valitud sõnas! : *oo*
+        Arvuti: Olete pakkunud 2 korda. Ning pakkunud tähti : a,o
+        Arvuti: Palun paku uus täht.
+        Mängija: k
+        -
+        **Arvuti kontrollib, kas pakutud täht on sõnas. Vastuseks saab true, sest sõnas "kool" on vähemalt 1 k täht.
+        -
+        Arvuti: Teie pakutud täht esines valitud sõnas! : koo*
+        Arvuti: Olete pakkunud 3 korda. Ning pakkunud tähti : a,o,k
+        Arvuti: Palun paku uus täht.
+        Mängija: r
+         -
+        **Arvuti kontrollib, kas pakutud täht on sõnas. Vastuseks saab false, sest sõnas "kool" ei ole ühtegi r tähte.
+        -
+        Arvuti: Teie pakutud tähte ei esine valitud sõnas! : koo*
+        Arvuti: Olete pakkunud 4 korda. Ning pakkunud tähti : a,o,k,r
+        Arvuti: Palun paku uus täht.
+        Mängija: l
+         -
+         **Arvuti kontrollib, kas pakutud täht on sõnas. Vastuseks saab true, sest sõnas "kool" on vähemalt 1 l täht.
+        -
+        Arvuti: Teie pakutud täht esines antud sõnas! : kool
+        Arvuti: Olete pakkunud 5 korda. Ning pakkunud tähti : a,o,k,r,l
+        -
+        Arvuti: Sõna on ära arvatud! Tubli!
+        -----------------------------------------------------------------------
+
+
+     */
+
+}
