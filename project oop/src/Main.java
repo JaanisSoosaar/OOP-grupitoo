d7f1b3f4fc63e2bf78e4b4937ef31cffa62c78bc
AnastasiaStarinchikova <116276064+AnastasiaStarinchikova@users.noreply.github.com>
2023-03-21 17:23:51 +0200

    Add files via upload

---
+++ project oop/src/Main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        mäng.go();
+
+    }
+
+}
